cbadc16705a7eb37c5a7e9c5cabcc588ab59fc0d
rvimys <rafiputraramadhan14@gmail.com>
2022-03-19 09:30:00 +0700

    Add Contoh1 controller.

---
+++ app/Controllers/Contoh1Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1Controller extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Rafi Putra Ramadhan, 
+        saya tinggal di daerah Pamulang Timur, 
+        dan olahraga yang saya sukai adalah Bulutangkis.";
+    }
+}
